78c15a746dc9f0910981f1444a1a3b347cb3f8ce
DilshanGDT <tharakadilshan506@gmail.com>
2025-02-13 18:49:41 +0530

    rotating sphere with globe texture - C++ file only

---
+++ Program_10/Rotating sphere with globe texture.cpp
@@ -0,0 +1,206 @@
+#include <gl\glut.h>
+#include <math.h>
+#include <SOIL2.h>
+#include <stdio.h>
+
+//variables to move the camera
+GLfloat camX = 0.0; GLfloat camY = 0.0; GLfloat camZ = 0.0;
+
+//variables to move the scene
+GLfloat sceRX = 0.0; GLfloat sceRY = 0.0; GLfloat sceRZ = 0.0;
+GLfloat sceTX = 0.0; GLfloat sceTY = 0.0; GLfloat sceTZ = 0.0;
+
+////variables to move the objects
+GLfloat objRX = 0.0; GLfloat objRY = 0.0; GLfloat objRZ = 0.0;
+GLfloat objTX = 0.0; GLfloat objTY = 0.0; GLfloat objTZ = 0.0;
+
+constexpr float PI = 3.14159265358979323846;
+
+
+// Angle for rotating the Earth
+float angle = 0.0f;
+
+//For texture image
+GLuint textureImage;
+
+//Read the image to texture image
+void loadTexture() {
+	// SOIL_load
+	textureImage = SOIL_load_OGL_texture(
+		"earthDayNight1.jpg",  // Replace with the path to your texture file
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS | SOIL_FLAG_INVERT_Y
+	);
+
+	if (!textureImage) {
+		printf("Texture loading failed: %s\n", SOIL_last_result());
+	}
+}
+
+void drawGrid() {
+	GLfloat step = 1.0f;
+	GLint line;
+
+	glBegin(GL_LINES);
+	for (line = -20; line <= 20; line += step) {
+		glVertex3f(line, -0.4, 20);
+		glVertex3f(line, -0.4, -20);
+
+		glVertex3f(20, -0.4, line);
+		glVertex3f(-20, -0.4, line);
+	}
+	glEnd();
+}
+
+void drawAxes() {
+
+	glBegin(GL_LINES);
+
+	glLineWidth(1.5);
+
+	glColor3f(1.0, 0.0, 0.0);
+	glVertex3f(-200, 0, 0);
+	glVertex3f(200, 0, 0);
+
+	glColor3f(0.0, 1.0, 0.0);
+	glVertex3f(0, -200, 0);
+	glVertex3f(0, 200, 0);
+
+	glColor3f(0.0, 0.0, 1.0);
+	glVertex3f(0, 0, -200);
+	glVertex3f(0, 0, 200);
+
+	glEnd();
+}
+
+
+void init(void) {
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearDepth(1.0);
+	glEnable(GL_TEXTURE_2D);
+	glEnable(GL_DEPTH_TEST);
+
+	//Enable Texture
+	loadTexture();
+}
+
+void drawSphere() {
+	GLUquadric* quad = gluNewQuadric();
+
+	//Bind the texture to the globe
+	gluQuadricTexture(quad, GL_TRUE);	//enable glu quadratic surface texture
+	glBindTexture(GL_TEXTURE_2D, textureImage);	// inde the texture image
+
+	gluSphere(quad, 1.0, 32, 32);
+
+	gluDeleteQuadric(quad);
+}
+
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glPushMatrix();
+	// camera orientation (eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ)
+	gluLookAt(0.0, 2.0 + camY, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
+	// move the scene (all the rendered environment) using keyboard keys
+	glTranslatef(sceTX, sceTY, sceTZ);
+	glRotatef(sceRY, 0.0, 1.0, 0.0);
+
+	// Rotate Earth
+	glRotatef(angle, 0, 1, 0);	// use y axis to rotate
+	glScalef(2.0, 2.0, 2.0);
+	drawSphere();
+
+	glPopMatrix();
+	glutSwapBuffers();
+}
+
+void timer(int value) {
+	// increase the rotation angle
+
+	if (angle >= 360)
+		angle = 0;
+	else 
+		angle = angle + 2;
+	
+	glutPostRedisplay();
+	glutTimerFunc(60, timer, 0);
+}
+
+
+void reshape(GLsizei w, GLsizei h) {
+	glViewport(0, 0, w, h);
+	GLfloat aspect_ratio = h == 0 ? w / 1 : (GLfloat)w / (GLfloat)h;
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	//Define the Perspective projection frustum 
+	// (FOV_in_vertical, aspect_ratio, z-distance to the near plane from the camera position, z-distance to far plane from the camera position)
+	gluPerspective(120.0, aspect_ratio, 1.0, 100.0);
+
+}
+
+void keyboardSpecial(int key, int x, int y) {
+	if (key == GLUT_KEY_UP)
+		camY += 1;
+
+	if (key == GLUT_KEY_DOWN)
+		camY -= 1;
+
+	if (key == GLUT_KEY_RIGHT)
+		sceTX += 1;
+
+	if (key == GLUT_KEY_LEFT)
+		sceTX -= 1;
+
+	glutPostRedisplay();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	if (key == 'l')
+		objRY += 1;
+
+	if (key == 'r')
+		objRY -= 1;
+
+	if (key == 'Z')
+		sceTZ += 1;
+
+	if (key == 'z')
+		sceTZ -= 1;
+
+	if (key == 'w')
+		sceTX += 1;
+
+	if (key == 's')
+		sceTX -= 1;
+
+	if (key == 'y')
+		sceRY += 1;
+
+	if (key == 'Y')
+		sceRY -= 1;
+
+	//glutPostRedisplay();
+}
+
+
+int main(void) {
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutInitWindowPosition(0, 0);
+	glutCreateWindow("Complex 3D Shapes");
+	glutKeyboardFunc(keyboard);
+	glutSpecialFunc(keyboardSpecial);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+
+	glutTimerFunc(30, timer, 0);
+
+	init();
+	glutMainLoop();
+	return 0;
+}
